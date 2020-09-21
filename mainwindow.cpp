6ee4141e4ec0c6dcd113baebb65bb69756a364fb
wsdassssss <68675769+wsdassssss@users.noreply.github.com>
2020-09-21 11:07:43 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,137 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QMessageBox"
+#include "QFontDialog"
+#include "QFont"
+#include "QColor"
+#include "QColorDialog"
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+    setWindowTitle("Notepadd");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionnew_triggered()
+{
+    file_path="";
+    ui->textEdit->setText("");
+}
+
+void MainWindow::on_actionopen_triggered()
+{
+    QString file_name=QFileDialog::getOpenFileName(this,"open the file");
+    QFile file(file_name);
+    file_path=file_name;
+    if(!file.open(QFile::ReadOnly |QFile::Text)){
+        QMessageBox::warning(this,"wrong","file not opne");
+        return;
+    }
+    QTextStream in(&file);
+    QString text=in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+    setWindowTitle(file_path);
+}
+
+void MainWindow::on_actionsave_triggered()
+{
+    QFile file(file_path);
+    if(!file.open(QFile::WriteOnly |QFile::Text)){
+        QMessageBox::warning(this,"wrong","file not save");
+        return;
+    }
+    QTextStream out(&file);
+    QString text=ui->textEdit->toPlainText();
+    out<<text;
+    file.flush();
+    file.close();
+}
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString file_name=QFileDialog::getSaveFileName(this,"Save As the file");
+    QFile file(file_name);
+    file_path=file_name;
+    if(!file.open(QFile::WriteOnly |QFile::Text)){
+        QMessageBox::warning(this,"wrong","file not save as");
+        return;
+    }
+    QTextStream out(&file);
+    QString text=ui->textEdit->toPlainText();
+    out<<text;
+    file.flush();
+    file.close();
+}
+
+void MainWindow::on_actioncut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actioncopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QString about_text="Author:FYL \nFunction:TextEdit \nTime:2020-0909-1618";
+    QMessageBox::about(this,"About notepad(jia)",about_text);
+}
+
+void MainWindow::on_actionFont_triggered()
+{
+    bool ok;
+    QFont font=QFontDialog::getFont(&ok,this);
+    if(ok){
+        ui->textEdit->setFont(font);
+    }else return;
+}
+
+void MainWindow::on_actionColor_triggered()
+{
+    QColor color=QColorDialog::getColor(Qt::white,this,"Choose color");
+    if(color.isValid()){
+        ui->textEdit->setTextColor(color);
+    }
+}
+
+void MainWindow::on_actionBackColor_triggered()
+{
+    QColor color=QColorDialog::getColor(Qt::white,this,"Choose color");
+    if(color.isValid()){
+        ui->textEdit->setTextBackgroundColor(color);
+    }
+}
+
+void MainWindow::on_actionBackground_color_triggered()
+{
+    QColor color=QColorDialog::getColor(Qt::white,this,"choose color");
+    //QColor color=QColorDialog::getColor(QColor(255,0,0));
+    if(color.isValid()){
+        ui->textEdit->setPalette(QPalette(color));
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,54 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "QFileDialog"
+#include "QFile"
+#include "QTextStream"
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionnew_triggered();
+
+    void on_actionopen_triggered();
+
+    void on_actionsave_triggered();
+
+    void on_actionSave_As_triggered();
+
+    void on_actioncut_triggered();
+
+    void on_actioncopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionAbout_triggered();
+
+    void on_actionFont_triggered();
+
+    void on_actionColor_triggered();
+
+    void on_actionBackColor_triggered();
+
+    void on_actionBackground_color_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    QString file_path;
+};
+
+#endif // MAINWINDOW_H
